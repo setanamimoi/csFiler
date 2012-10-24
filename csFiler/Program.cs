0091bc2e12cde9cb789c00806280003ce565f9d7
setanamimoi <setanamimoi@gmail.com>
2012-10-25 08:58:23 +0900

    メインプロジェクトの作成

---
+++ csFiler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csFiler
+{
+    /// <summary>
+    /// エントリポイントを定義するクラス
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// エントリポイント
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+        }
+    }
+}
