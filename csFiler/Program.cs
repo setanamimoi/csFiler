660ed193abd3ab3c28ec248653f7814b3721e14c
setanamimoi <setanamimoi@gmail.com>
2012-10-25 16:58:01 +0900

    タスクトレイの実装

--- csFiler/Program.cs
+++ csFiler/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-using System.Windows;
+using Wpf = System.Windows;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
 
 namespace csFiler
 {
@@ -14,7 +17,61 @@ namespace csFiler
         [STAThread]
         public static void Main()
         {
-            new Application().Run(new FilerWindow());
+            var iconPath = Path.Combine(Application.StartupPath, "csFiler.ico");
+            using (var icon = new Icon(iconPath))
+            using (var commandMenu = new ToolStripMenuItem()
+                {
+                    Text = "コマンド入力",
+                })
+            using (var exitMenuItem = new ToolStripMenuItem()
+                {
+                    Text = "終了",
+                })
+            using (var contextMenu = new ContextMenuStrip())
+            using (var taskTray = new NotifyIcon()
+                {
+                    Icon = icon,
+                    Text = "csFiler",
+                    ContextMenuStrip = contextMenu,
+                })
+            {
+                contextMenu.Items.AddRange(
+                    new ToolStripItem [] { 
+                        commandMenu,
+                        exitMenuItem,
+                    });
+                contextMenu.Click += new EventHandler((sender, e) =>
+                {
+                    new FilerWindow().Show();
+                });
+                exitMenuItem.Click += new EventHandler((sender, e) =>
+                {
+                    Wpf.Application.Current.Shutdown();
+                });
+
+                var application = new Wpf.Application()
+                {
+                    ShutdownMode = Wpf.ShutdownMode.OnExplicitShutdown,
+                };
+                application.DispatcherUnhandledException += new Wpf.Threading.DispatcherUnhandledExceptionEventHandler((sender, e) =>
+                {
+                    MessageBox.Show(
+                        string.Concat("想定外のエラーが発生しました。",
+                        Environment.NewLine, e.Exception.Message), "csFiler");
+                    e.Handled = true;
+                });
+                application.Exit += new Wpf.ExitEventHandler((sender, e) =>
+                {
+                    taskTray.Dispose();
+                    contextMenu.Dispose();
+                    exitMenuItem.Dispose();
+                    commandMenu.Dispose();
+                    icon.Dispose();
+                });
+
+                taskTray.Visible = true;
+                application.Run();
+            }
         }
     }
 }
