a6b6930b2eca388355be42a5e1e6f1a55d9142f5
setanamimoi <setanamimoi@gmail.com>
2012-11-03 23:14:17 +0900

    Program.+Main():voidのリファクタリング

--- csFiler/Program.cs
+++ csFiler/Program.cs
@@ -22,55 +22,45 @@ namespace csFiler
         {
             var iconPath = Path.Combine(Application.StartupPath, "csFiler.ico");
             using (var icon = new Icon(iconPath))
-            using (var commandMenu = new ToolStripMenuItem()
-                {
-                    Text = "コマンド入力",
-                })
-            using (var exitMenuItem = new ToolStripMenuItem()
-                {
-                    Text = "終了",
-                })
+            using (var commandMenu = new ToolStripMenuItem("コマンド入力"))
+            using (var exitMenuItem = new ToolStripMenuItem("終了"))
             using (var contextMenu = new ContextMenuStrip())
-            using (var taskTray = new NotifyIcon()
-                {
-                    Icon = icon,
-                    Text = "csFiler",
-                    ContextMenuStrip = contextMenu,
-                })
+            using (var taskTray = new NotifyIcon())
             using (var hotKeyLoop = new HotKeyMessageLoop())
             {
-                HotKey openCommand = new HotKey(
+                taskTray.Icon = icon;
+                taskTray.Text = "csFiler";
+                taskTray.ContextMenuStrip = contextMenu;
+
+                var openCommand = new HotKey(
                     new Action(() => new FilerWindow().Show()), 
                     Key.V, ModifierKeys.Windows);
 
                 hotKeyLoop.Add(openCommand);
 
                 contextMenu.Items.AddRange(
-                    new ToolStripItem [] { 
+                    new ToolStripItem []
+                    { 
                         commandMenu,
                         exitMenuItem,
                     });
-                contextMenu.Click += new EventHandler((sender, e) =>
-                {
-                    openCommand.Action.Invoke();
-                });
-                exitMenuItem.Click += new EventHandler((sender, e) =>
-                {
-                    Wpf.Application.Current.Shutdown();
-                });
+                contextMenu.Click += (sender, e) => openCommand.Action.Invoke();
+                exitMenuItem.Click += (sender, e) => Wpf.Application.Current.Shutdown();
 
                 var application = new Wpf.Application()
                 {
                     ShutdownMode = Wpf.ShutdownMode.OnExplicitShutdown,
                 };
-                application.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler((sender, e) =>
+
+                application.DispatcherUnhandledException += (sender, e) =>
                 {
                     MessageBox.Show(
                         string.Concat("想定外のエラーが発生しました。",
                         Environment.NewLine, e.Exception.Message), "csFiler");
                     e.Handled = true;
-                });
-                application.Exit += new Wpf.ExitEventHandler((sender, e) =>
+                };
+
+                application.Exit += (sender, e) =>
                 {
                     hotKeyLoop.Dispose();
                     taskTray.Dispose();
@@ -78,7 +68,7 @@ namespace csFiler
                     exitMenuItem.Dispose();
                     commandMenu.Dispose();
                     icon.Dispose();
-                });
+                };
 
                 taskTray.Visible = true;
                 application.Run();
