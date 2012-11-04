1276419816bd60a789fa90707f296563d37ce949
setanamimoi <setanamimoi@gmail.com>
2012-11-04 11:46:15 +0900

    キーボードでインテリセンスへアクセスできるように変更

--- csFiler/FilerWindow.xaml.cs
+++ csFiler/FilerWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace csFiler
 {
@@ -28,9 +30,32 @@ namespace csFiler
             this.intellisenceListBox.ItemsSource = intellisenceItemsSource;
         }
 
+        private void Window_Deactivated(object sender, System.EventArgs e)
+        {
+            this.intellisence.IsOpen = false;
+        }
+
         private void processTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (Keyboard.IsKeyDown(Key.Enter) == true)
+            if (Keyboard.IsKeyDown(Key.Space) == true && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+            {
+                this.intellisence.IsOpen = true;
+                e.Handled = true;
+            }
+            else if (Keyboard.IsKeyDown(Key.Down) == true)
+            {
+                if (this.intellisence.IsOpen == false)
+                {
+                    return;
+                }
+
+                this.intellisenceListBox.Focus();
+                this.intellisenceListBox.SelectedIndex = 0;
+                var selectedItem = this.intellisenceListBox.ItemContainerGenerator.ContainerFromIndex(this.intellisenceListBox.SelectedIndex) as ListBoxItem;
+                selectedItem.Focus();
+                e.Handled = true;
+            }
+            else if (Keyboard.IsKeyDown(Key.Enter) == true)
             {
                 var process = new ProcessStartInfo();
                 process.BindFromCommandLine(this.processTextBox.Text);
@@ -40,16 +65,21 @@ namespace csFiler
                 this.Close();
                 e.Handled = true;
             }
-            else if (Keyboard.IsKeyDown(Key.Space) == true && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
-            {
-                this.intellisence.IsOpen = true;
-                e.Handled = true;
-            }
         }
 
-        private void Window_Deactivated(object sender, System.EventArgs e)
+        private void intellisenceListBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            this.intellisence.IsOpen = false;
+            if (Keyboard.IsKeyDown(Key.Up) == true)
+            {
+                if (this.intellisenceListBox.SelectedIndex != 0)
+                {
+                    return;
+                }
+
+                this.intellisenceListBox.SelectedIndex = -1;
+                this.processTextBox.Focus();
+                e.Handled = true;
+            }
         }
     }
 }
